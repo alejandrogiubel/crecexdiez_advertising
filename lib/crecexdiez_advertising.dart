ba2b953753f5f3117400e8ec9bde3a0a29c9bfa3
Alejandro Giubel <agiubel@gmail.com>
2021-05-19 17:52:06 -0400

    Pub dev issue for score

--- lib/crecexdiez_advertising.dart
+++ lib/crecexdiez_advertising.dart
@@ -14,56 +14,56 @@ class Crecex10Advertising extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getImageFromCrecex10(),
-      builder: (BuildContext _, AsyncSnapshot<List<int>?> snapShot) {
-        if (snapShot.hasData) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: width.toDouble(),
-                height: height.toDouble(),
-                child: Stack(
-                  children: [
-                    Image.memory(snapShot.data as Uint8List),
-                    Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        onTap: () async {
-                          await launch('https://crecexdiez.com/s/l/$id/${width}x$height');
-                        },
-                      ),
-                    )
-                  ],
+        future: getImageFromCrecex10(),
+        builder: (BuildContext _, AsyncSnapshot<List<int>?> snapShot) {
+          if (snapShot.hasData) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                    width: width.toDouble(),
+                    height: height.toDouble(),
+                    child: Stack(
+                      children: [
+                        Image.memory(snapShot.data as Uint8List),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () async {
+                              await launch(
+                                  'https://crecexdiez.com/s/l/$id/${width}x$height');
+                            },
+                          ),
+                        )
+                      ],
+                    )),
+                InkWell(
+                  borderRadius: BorderRadius.circular(1000),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text('Promocionado por CreceXDiez'),
+                  ),
+                  onTap: () async {
+                    await launch('https://crecexdiez.com');
+                  },
                 )
-              ),
-              InkWell(
-                borderRadius: BorderRadius.circular(1000),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  child: Text('Promocionado por CreceXDiez'),
-                ),
-                onTap: () async {
-                  await launch('https://crecexdiez.com');
-                },
-              )
-            ],
-          );
-        } else {
-          return Container(
-            width: 50,
-            height: 50,
-            child: CircularProgressIndicator()
-          );
-        }
-      }
-    );
+              ],
+            );
+          } else {
+            return Container(
+                width: 50, height: 50, child: CircularProgressIndicator());
+          }
+        });
   }
 
-  Future<List<int>?>getImageFromCrecex10() async {
+  ///Return a byte list from crecexdiez platform with the generated ads image.
+  Future<List<int>?> getImageFromCrecex10() async {
     Response<List<int>> rs;
-    rs = await Dio().get<List<int>>('https://crecexdiez.com/s/i/$id/${width}x$height',
-      options: Options(responseType: ResponseType.bytes), // set responseType to `bytes`
+    rs = await Dio().get<List<int>>(
+      'https://crecexdiez.com/s/i/$id/${width}x$height',
+      options: Options(
+          responseType: ResponseType.bytes), // set responseType to `bytes`
     );
     return rs.data;
   }
